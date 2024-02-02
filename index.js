530ae5b0826b994a93196c830c11d44668c6e105
Dagim Alemu <dagimalemux@gmail.com>
2024-02-02 13:15:58 +0300

    commit

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const humberger = document.querySelector(".line")
 
 humberger.addEventListener("click", () => {
-    this.classList.toggle("close");
-    overlay.classList.toggle("overlay");
+    
 }) 
