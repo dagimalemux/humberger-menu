084d02e15b876fbb6c5df0924cb20ab59b38799e
Dagim Alemu <dagimalemux@gmail.com>
2024-02-02 15:03:10 +0300

    commit

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-const humberger = document.querySelector(".line")
-
-humberger.addEventListener("click", () => {
-    
-}) 
+// Event listener for opening the navigation bar
+document.querySelectorAll('.lines').forEach(item => {
+    item.addEventListener('click', event => {
+      document.querySelector('nav').style.display = 'flex';
+    });
+  });
+  
+  // Event listener for closing the navigation bar
+  document.querySelectorAll('.line').forEach(item => {
+    item.addEventListener('click', event => {
+      document.querySelector('nav').style.display = 'none';
+    });
+  });
+  
